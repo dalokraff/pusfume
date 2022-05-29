fc274bc850296dbb3c453a02e82e0f7d3ee5d809
dalokraff <87997987+dalokraff@users.noreply.github.com>
2022-05-29 13:43:39 -0500

    separated main body from whisker/hair mesh and added emissive to eye and backpack
    some LOD issue with the whiskers/hair mesh

--- scripts/mods/pusfume/pusfume.lua
+++ scripts/mods/pusfume/pusfume.lua
@@ -2,6 +2,7 @@ local mod = get_mod("pusfume")
 mod:dofile("scripts/mods/pusfume/utils/network_tables")
 mod:dofile("scripts/mods/pusfume/utils/hooks")
 mod:dofile("scripts/mods/pusfume/utils/interaction")
+mod:dofile("scripts/mods/pusfume/pusfume_attachment_node")
 -- Your mod code goes here.
 -- https://vmf-docs.verminti.de
 
@@ -18,6 +19,9 @@ mod:command("spawn_pusfume", "", function()
     local unit_template_name = "interaction_unit"
     local extension_init_data = {}
     local unit, go_id = unit_spawner:spawn_network_unit("units/pusfume/pusfume_inn", unit_template_name, extension_init_data, position, rotation)
+    local unit2 = Managers.state.unit_spawner:spawn_local_unit("units/pusfume/pusfume_inn_fur", position, rotation)
+    Unit.disable_animation_state_machine(unit2)
+    AttachmentUtils.link(world, unit, unit2, AttachmentNodeLinking.pusfume)
 end)
 
 mod:command("spawn_pusfume_no_extension", "", function() 
@@ -30,4 +34,18 @@ mod:command("spawn_pusfume_no_extension", "", function()
     local unit_template_name = "interaction_unit"
     local extension_init_data = {}
     local unit, go_id = unit_spawner:spawn_local_unit("units/pusfume/pusfume_inn", position, rotation)
+    local unit2 = Managers.state.unit_spawner:spawn_local_unit("units/pusfume/pusfume_inn_fur", position, rotation)
+    Unit.disable_animation_state_machine(unit2)
+    AttachmentUtils.link(world, unit, unit2, AttachmentNodeLinking.pusfume)
 end)
+
+
+-- local world = Managers.world:world("level_world")
+-- local player = Managers.player:local_player()
+-- local player_unit = player.player_unit
+-- local position = Unit.local_position(player_unit, 0) 
+-- local rotation = Unit.local_rotation(player_unit, 0)
+-- local unit1 = Managers.state.unit_spawner:spawn_local_unit("units/pusfume/pusfume_inn", position, rotation)
+-- local unit2 = Managers.state.unit_spawner:spawn_local_unit("units/pusfume/pusfume_inn_fur", position, rotation)
+-- Unit.disable_animation_state_machine(unit2)
+-- AttachmentUtils.link(world, unit1, unit2, AttachmentNodeLinking.pusfume)
---
+++ scripts/mods/pusfume/pusfume_attachment_node.lua
@@ -0,0 +1,596 @@
+local mod = get_mod("pusfume")
+
+AttachmentNodeLinking.pusfume = {
+    {
+        target = 0,
+        source = "root_point",
+    },
+    {
+        target = "DEF-shoulder_L",
+        source = "DEF-shoulder_L",
+    },
+    {
+        target = "DEF-shoulder_L_end",
+        source = "DEF-shoulder_L_end",
+    },
+    {
+        target = "DEF-upper_arm_L",
+        source = "DEF-upper_arm_L",
+    },
+    {
+        target = "DEF-upper_arm_L_001",
+        source = "DEF-upper_arm_L_001",
+    },
+    {
+        target = "DEF-forearm_L",
+        source = "DEF-forearm_L",
+    },
+    {
+        target = "DEF-forearm_L_001",
+        source = "DEF-forearm_L_001",
+    },
+    {
+        target = "DEF-hand_L",
+        source = "DEF-hand_L",
+    },
+    {
+        target = "DEF-f_index_01_L",
+        source = "DEF-f_index_01_L",
+    },
+    {
+        target = "DEF-f_index_02_L",
+        source = "DEF-f_index_02_L",
+    },
+    {
+        target = "DEF-f_index_03_L",
+        source = "DEF-f_index_03_L",
+    },
+    {
+        target = "DEF-f_index_03_L_end",
+        source = "DEF-f_index_03_L_end",
+    },
+    {
+        target = "DEF-thumb_01_L",
+        source = "DEF-thumb_01_L",
+    },
+    {
+        target = "DEF-thumb_02_L",
+        source = "DEF-thumb_02_L",
+    },
+    {
+        target = "DEF-thumb_03_L",
+        source = "DEF-thumb_03_L",
+    },
+    {
+        target = "DEF-thumb_03_L_end",
+        source = "DEF-thumb_03_L_end",
+    },
+    {
+        target = "DEF-palm_01_L",
+        source = "DEF-palm_01_L",
+    },
+    {
+        target = "DEF-palm_01_L_end",
+        source = "DEF-palm_01_L_end",
+    },
+    {
+        target = "DEF-f_middle_01_L",
+        source = "DEF-f_middle_01_L",
+    },
+    {
+        target = "DEF-f_middle_02_L",
+        source = "DEF-f_middle_02_L",
+    },
+    {
+        target = "DEF-f_middle_03_L",
+        source = "DEF-f_middle_03_L",
+    },
+    {
+        target = "DEF-f_middle_03_L_end",
+        source = "DEF-f_middle_03_L_end",
+    },
+    {
+        target = "DEF-palm_02_L",
+        source = "DEF-palm_02_L",
+    },
+    {
+        target = "DEF-palm_02_L_end",
+        source = "DEF-palm_02_L_end",
+    },
+    {
+        target = "DEF-f_ring_01_L",
+        source = "DEF-f_ring_01_L",
+    },
+    {
+        target = "DEF-f_ring_02_L",
+        source = "DEF-f_ring_02_L",
+    },
+    {
+        target = "DEF-f_ring_03_L",
+        source = "DEF-f_ring_03_L",
+    },
+    {
+        target = "DEF-f_ring_03_L_end",
+        source = "DEF-f_ring_03_L_end",
+    },
+    {
+        target = "DEF-palm_03_L",
+        source = "DEF-palm_03_L",
+    },
+    {
+        target = "DEF-palm_03_L_end",
+        source = "DEF-palm_03_L_end",
+    },
+    {
+        target = "DEF-f_pinky_01_L",
+        source = "DEF-f_pinky_01_L",
+    },
+    {
+        target = "DEF-f_pinky_02_L",
+        source = "DEF-f_pinky_02_L",
+    },
+    {
+        target = "DEF-f_pinky_03_L",
+        source = "DEF-f_pinky_03_L",
+    },
+    {
+        target = "DEF-f_pinky_03_L_end",
+        source = "DEF-f_pinky_03_L_end",
+    },
+    {
+        target = "DEF-palm_04_L",
+        source = "DEF-palm_04_L",
+    },
+    {
+        target = "DEF-palm_04_L_end",
+        source = "DEF-palm_04_L_end",
+    },
+    {
+        target = "DEF-shoulder_R",
+        source = "DEF-shoulder_R",
+    },
+    {
+        target = "DEF-shoulder_R_end",
+        source = "DEF-shoulder_R_end",
+    },
+    {
+        target = "DEF-upper_arm_R",
+        source = "DEF-upper_arm_R",
+    },
+    {
+        target = "DEF-upper_arm_R_001",
+        source = "DEF-upper_arm_R_001",
+    },
+    {
+        target = "DEF-forearm_R",
+        source = "DEF-forearm_R",
+    },
+    {
+        target = "DEF-forearm_R_001",
+        source = "DEF-forearm_R_001",
+    },
+    {
+        target = "DEF-hand_R",
+        source = "DEF-hand_R",
+    },
+    {
+        target = "DEF-f_index_01_R",
+        source = "DEF-f_index_01_R",
+    },
+    {
+        target = "DEF-f_index_02_R",
+        source = "DEF-f_index_02_R",
+    },
+    {
+        target = "DEF-f_index_03_R",
+        source = "DEF-f_index_03_R",
+    },
+    {
+        target = "DEF-f_index_03_R_end",
+        source = "DEF-f_index_03_R_end",
+    },
+    {
+        target = "DEF-thumb_01_R",
+        source = "DEF-thumb_01_R",
+    },
+    {
+        target = "DEF-thumb_02_R",
+        source = "DEF-thumb_02_R",
+    },
+    {
+        target = "DEF-thumb_03_R",
+        source = "DEF-thumb_03_R",
+    },
+    {
+        target = "DEF-thumb_03_R_end",
+        source = "DEF-thumb_03_R_end",
+    },
+    {
+        target = "DEF-palm_01_R",
+        source = "DEF-palm_01_R",
+    },
+    {
+        target = "DEF-palm_01_R_end",
+        source = "DEF-palm_01_R_end",
+    },
+    {
+        target = "DEF-f_middle_01_R",
+        source = "DEF-f_middle_01_R",
+    },
+    {
+        target = "DEF-f_middle_02_R",
+        source = "DEF-f_middle_02_R",
+    },
+    {
+        target = "DEF-f_middle_03_R",
+        source = "DEF-f_middle_03_R",
+    },
+    {
+        target = "DEF-f_middle_03_R_end",
+        source = "DEF-f_middle_03_R_end",
+    },
+    {
+        target = "DEF-palm_02_R",
+        source = "DEF-palm_02_R",
+    },
+    {
+        target = "DEF-palm_02_R_end",
+        source = "DEF-palm_02_R_end",
+    },
+    {
+        target = "DEF-f_ring_01_R",
+        source = "DEF-f_ring_01_R",
+    },
+    {
+        target = "DEF-f_ring_02_R",
+        source = "DEF-f_ring_02_R",
+    },
+    {
+        target = "DEF-f_ring_03_R",
+        source = "DEF-f_ring_03_R",
+    },
+    {
+        target = "DEF-f_ring_03_R_end",
+        source = "DEF-f_ring_03_R_end",
+    },
+    {
+        target = "DEF-palm_03_R",
+        source = "DEF-palm_03_R",
+    },
+    {
+        target = "DEF-palm_03_R_end",
+        source = "DEF-palm_03_R_end",
+    },
+    {
+        target = "DEF-f_pinky_01_R",
+        source = "DEF-f_pinky_01_R",
+    },
+    {
+        target = "DEF-f_pinky_02_R",
+        source = "DEF-f_pinky_02_R",
+    },
+    {
+        target = "DEF-f_pinky_03_R",
+        source = "DEF-f_pinky_03_R",
+    },
+    {
+        target = "DEF-f_pinky_03_R_end",
+        source = "DEF-f_pinky_03_R_end",
+    },
+    {
+        target = "DEF-palm_04_R",
+        source = "DEF-palm_04_R",
+    },
+    {
+        target = "DEF-palm_04_R_end",
+        source = "DEF-palm_04_R_end",
+    },
+    {
+        target = "prop_R",
+        source = "prop_R",
+    },
+    {
+        target = "prop_R_end",
+        source = "prop_R_end",
+    },
+    {
+        target = "DEF-breast_L",
+        source = "DEF-breast_L",
+    },
+    {
+        target = "DEF-breast_L_end",
+        source = "DEF-breast_L_end",
+    },
+    {
+        target = "DEF-breast_R",
+        source = "DEF-breast_R",
+    },
+    {
+        target = "DEF-breast_R_end",
+        source = "DEF-breast_R_end",
+    },
+    {
+        target = "DEF-pelvis_L",
+        source = "DEF-pelvis_L",
+    },
+    {
+        target = "DEF-pelvis_L_end",
+        source = "DEF-pelvis_L_end",
+    },
+    {
+        target = "DEF-pelvis_R",
+        source = "DEF-pelvis_R",
+    },
+    {
+        target = "DEF-pelvis_R_end",
+        source = "DEF-pelvis_R_end",
+    },
+    {
+        target = "DEF-tail",
+        source = "DEF-tail",
+    },
+    {
+        target = "DEF-tail_001",
+        source = "DEF-tail_001",
+    },
+    {
+        target = "DEF-tail_002",
+        source = "DEF-tail_002",
+    },
+    {
+        target = "DEF-tail_003",
+        source = "DEF-tail_003",
+    },
+    {
+        target = "DEF-tail_004",
+        source = "DEF-tail_004",
+    },
+    {
+        target = "DEF-tail_005",
+        source = "DEF-tail_005",
+    },
+    {
+        target = "DEF-tail_006",
+        source = "DEF-tail_006",
+    },
+    {
+        target = "DEF-tail_007",
+        source = "DEF-tail_007",
+    },
+    {
+        target = "DEF-tail_008",
+        source = "DEF-tail_008",
+    },
+    {
+        target = "DEF-tail_009",
+        source = "DEF-tail_009",
+    },
+    {
+        target = "DEF-tail_010",
+        source = "DEF-tail_010",
+    },
+    {
+        target = "DEF-tail_011",
+        source = "DEF-tail_011",
+    },
+    {
+        target = "DEF-tail_012",
+        source = "DEF-tail_012",
+    },
+    {
+        target = "DEF-tail_013",
+        source = "DEF-tail_013",
+    },
+    {
+        target = "DEF-tail_014",
+        source = "DEF-tail_014",
+    },
+    {
+        target = "DEF-tail_015",
+        source = "DEF-tail_015",
+    },
+    {
+        target = "DEF-tail_015_end",
+        source = "DEF-tail_015_end",
+    },
+    {
+        target = "DEF-thigh_L",
+        source = "DEF-thigh_L",
+    },
+    {
+        target = "DEF-thigh_L_001",
+        source = "DEF-thigh_L_001",
+    },
+    {
+        target = "DEF-shin_L",
+        source = "DEF-shin_L",
+    },
+    {
+        target = "DEF-shin_L_001",
+        source = "DEF-shin_L_001",
+    },
+    {
+        target = "DEF-foot_L",
+        source = "DEF-foot_L",
+    },
+    {
+        target = "DEF-foot_L_001",
+        source = "DEF-foot_L_001",
+    },
+    {
+        target = "DEF-toe_L",
+        source = "DEF-toe_L",
+    },
+    {
+        target = "DEF-toe_L_end",
+        source = "DEF-toe_L_end",
+    },
+    {
+        target = "DEF-thigh_R",
+        source = "DEF-thigh_R",
+    },
+    {
+        target = "DEF-thigh_R_001",
+        source = "DEF-thigh_R_001",
+    },
+    {
+        target = "DEF-shin_R",
+        source = "DEF-shin_R",
+    },
+    {
+        target = "DEF-shin_R_001",
+        source = "DEF-shin_R_001",
+    },
+    {
+        target = "DEF-foot_R",
+        source = "DEF-foot_R",
+    },
+    {
+        target = "DEF-foot_R_001",
+        source = "DEF-foot_R_001",
+    },
+    {
+        target = "DEF-toe_R",
+        source = "DEF-toe_R",
+    },
+    {
+        target = "DEF-toe_R_end",
+        source = "DEF-toe_R_end",
+    },
+    {
+        target = "jezzail",
+        source = "jezzail",
+    },
+    {
+        target = "jezzail_end",
+        source = "jezzail_end",
+    },
+    {
+        target = "root",
+        source = "root",
+    },
+    {
+        target = "DEF-spine",
+        source = "DEF-spine",
+    },
+    {
+        target = "DEF-spine_001",
+        source = "DEF-spine_001",
+    },
+    {
+        target = "DEF-spine_002",
+        source = "DEF-spine_002",
+    },
+    {
+        target = "DEF-spine_003",
+        source = "DEF-spine_003",
+    },
+    {
+        target = "DEF-spine_004",
+        source = "DEF-spine_004",
+    },
+    {
+        target = "DEF-spine_005",
+        source = "DEF-spine_005",
+    },
+    {
+        target = "DEF-spine_006",
+        source = "DEF-spine_006",
+    },
+    {
+        target = "headjaw",
+        source = "headjaw",
+    },
+    {
+        target = "tongue",
+        source = "tongue",
+    },
+    {
+        target = "tongue_001",
+        source = "tongue_001",
+    },
+    {
+        target = "tongue_002",
+        source = "tongue_002",
+    },
+    {
+        target = "tongue_003",
+        source = "tongue_003",
+    },
+    {
+        target = "tongue_003_end",
+        source = "tongue_003_end",
+    },
+    {
+        target = "eye_l",
+        source = "eye_l",
+    },
+    {
+        target = "eye_l_end",
+        source = "eye_l_end",
+    },
+    {
+        target = "eye_r",
+        source = "eye_r",
+    },
+    {
+        target = "eye_r_end",
+        source = "eye_r_end",
+    },
+    {
+        target = "mctrl_snout",
+        source = "mctrl_snout",
+    },
+    {
+        target = "mctrl_snout_end",
+        source = "mctrl_snout_end",
+    },
+    {
+        target = "pf_eye_L",
+        source = "pf_eye_L",
+    },
+    {
+        target = "pf_eye_L_end",
+        source = "pf_eye_L_end",
+    },
+    {
+        target = "eyelidtop_R",
+        source = "eyelidtop_R",
+    },
+    {
+        target = "eyelidtop_R_end",
+        source = "eyelidtop_R_end",
+    },
+    {
+        target = "eyelidbot_R",
+        source = "eyelidbot_R",
+    },
+    {
+        target = "eyelidbot_R_end",
+        source = "eyelidbot_R_end",
+    },
+    {
+        target = "pf_toolbag",
+        source = "pf_toolbag",
+    },
+    {
+        target = "pf_toolbag_end",
+        source = "pf_toolbag_end",
+    },
+    {
+        target = "pf_bag_L",
+        source = "pf_bag_L",
+    },
+    {
+        target = "pf_bag_L_end",
+        source = "pf_bag_L_end",
+    },
+    {
+        target = "props_furniture",
+        source = "props_furniture",
+    },
+    {
+        target = "props_furniture_end",
+        source = "props_furniture_end",
+    },
+}
