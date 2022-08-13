18a568c7058d6a712966501466f386e4ef651d29
dalokraff <87997987+dalokraff@users.noreply.github.com>
2022-08-13 16:01:44 -0500

    updated to include dialog for all chars and fixed attenuation of sound files

--- scripts/mods/pusfume/utils/conversation.lua
+++ scripts/mods/pusfume/utils/conversation.lua
@@ -76,6 +76,210 @@ mod.pusfume_conversations = {
             "pusfume",
         }
     },
+    {  
+        dwarf_ranger = {},
+        pusfume = {},
+        lines = {
+            "psf_b_01_1",
+			"pdr_cog_conversation_two_01",
+			"psf_b_01_2",
+            "pdr_dr_future_fivec_03",
+            "psf_b_01_3",
+        },
+        order = {
+            "pusfume",
+            "dwarf_ranger",
+            "pusfume",
+            "dwarf_ranger",
+            "pusfume",
+        }
+    },
+    {  
+        dwarf_ranger = {},
+        pusfume = {},
+        lines = {
+            "psf_b_02_1",
+			"pdr_hub_lohner_conversation_eight_01",
+			"psf_b_02_2",
+            "pdr_de_bless_conversation_twentyseven_02",
+        },
+        order = {
+            "pusfume",
+            "dwarf_ranger",
+            "pusfume",
+            "dwarf_ranger",
+        }
+    },
+    {  
+        dwarf_ranger = {},
+        pusfume = {},
+        lines = {
+            "psf_b_03_1",
+			"pdr_bw_backstory_five_02",
+			"psf_b_03_2",
+        },
+        order = {
+            "pusfume",
+            "dwarf_ranger",
+            "pusfume",
+        }
+    },
+    {  
+        dwarf_ranger = {},
+        pusfume = {},
+        lines = {
+            "psf_b_04_1",
+			"pdr_cog_conversation_twentytwo_02",
+			"psf_b_04_2",
+        },
+        order = {
+            "pusfume",
+            "dwarf_ranger",
+            "pusfume",
+        }
+    },
+    {  
+        dwarf_ranger = {},
+        pusfume = {},
+        lines = {
+            "psf_b_05_1",
+			"pdr_bless_conversation_ten_02",
+			"psf_b_05_2",
+        },
+        order = {
+            "pusfume",
+            "dwarf_ranger",
+            "pusfume",
+        }
+    },
+    {  
+        way_watcher = {},
+        pusfume = {},
+        lines = {
+            "psf_ker_01_1",
+			"pwe_st_woods_conversation_ten_02",
+        },
+        order = {
+            "pusfume",
+            "way_watcher",
+        }
+    },
+    {  
+        way_watcher = {},
+        pusfume = {},
+        lines = {
+            "psf_ker_02_1",
+			"pwe_es_future_four_03",
+            "psf_ker_02_2",
+			"pwe_st_woods_conversation_one_02",
+            "psf_ker_02_3",
+        },
+        order = {
+            "pusfume",
+            "way_watcher",
+            "pusfume",
+            "way_watcher",
+            "pusfume",
+        }
+    },
+    {  
+        way_watcher = {},
+        pusfume = {},
+        lines = {
+            "psf_ker_03_1",
+			"pwe_bw_future_five_02",
+            "psf_ker_03_2",
+			"pwe_st_woods_conversation_fifteen_02",
+            "psf_ker_03_3",
+        },
+        order = {
+            "pusfume",
+            "way_watcher",
+            "pusfume",
+            "way_watcher",
+            "pusfume",
+        }
+    },
+    {  
+        witch_hunter = {},
+        pusfume = {},
+        lines = {
+            "psf_saltz_01_1",
+			"pwh_cog_conversation_twentytwo_01",
+        },
+        order = {
+            "pusfume",
+            "witch_hunter",
+        }
+    },
+    {  
+        witch_hunter = {},
+        pusfume = {},
+        lines = {
+            "psf_saltz_02_1",
+			"pwh_comet_conversation_eight_02",
+        },
+        order = {
+            "pusfume",
+            "witch_hunter",
+        }
+    },
+    {  
+        witch_hunter = {},
+        pusfume = {},
+        lines = {
+            "psf_saltz_03_1",
+			"pwh_cog_conversation_twenty_03",
+            "psf_saltz_03_2",
+			"pwh_wp_bless_conversation_six_02",
+            "psf_saltz_03_3",
+        },
+        order = {
+            "pusfume",
+            "witch_hunter",
+            "pusfume",
+            "witch_hunter",
+            "pusfume",
+        }
+    },
+    {  
+        witch_hunter = {},
+        pusfume = {},
+        lines = {
+            "pwh_bless_conversation_thirteen_03",
+			"psf_saltz_04_1",
+        },
+        order = {
+            "witch_hunter",
+            "pusfume",
+        }
+    },
+    {  
+        bright_wizard = {},
+        pusfume = {},
+        lines = {
+            "psf_s_01_1",
+			"pbw_fleur_conversation_sixteen_02",
+            "psf_s_01_2",
+        },
+        order = {
+            "pusfume",
+            "bright_wizard",
+            "pusfume",
+        }
+    },
+    {  
+        bright_wizard = {},
+        pusfume = {},
+        lines = {
+            "psf_s_02_1",
+			"pbw_es_future_one_04",
+        },
+        order = {
+            "pusfume",
+            "bright_wizard",
+        }
+    },
     
 }
 
